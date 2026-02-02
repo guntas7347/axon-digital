9364db4e129a18f6246aba1d9995388fdc9857a0
Guntas Singh <guntas7347@gmail.com>
2026-02-02 16:10:04 +0530

    work update

--- lib/recentWorks.ts
+++ lib/recentWorks.ts
@@ -1,64 +1,64 @@
 export const recentWorks = [
-  {
-    slug: "smilinshack",
-    href: "https://www.guntassandhu.com/works/smilinshack",
-    title: "SmilinShack Booth",
-    description: "Modern photo booth site",
-    image:
-      "https://res.cloudinary.com/dfdurnxxn/video/upload/so_0,w_800,q_auto,f_webp/v1769846461/guntas.dev/i8f1zb7f8plofdofrchl.webp",
-    tags: ["Next.js", "SEO", "MDX"],
-    video: "/smilinshack.mp4",
-  },
-  {
-    slug: "hometechsystems",
-    href: "https://www.guntassandhu.com/works/hometechsystems",
-    title: "HomeTech Systems",
-    description: "Security automation web",
-    image:
-      "https://res.cloudinary.com/dfdurnxxn/video/upload/so_0,w_800,q_auto,f_webp/v1769846462/guntas.dev/ydn6l1lxtw2whuzekpb8.webp",
-    tags: ["Branding", "Next.js", "UI"],
-    video: "/hometechsystem.mp4",
-  },
-  {
-    slug: "srt-transport-inc",
-    href: "https://www.guntassandhu.com/works/srt-transport-inc",
-    title: "SRT Transport",
-    description: "Logistics business site",
-    image:
-      "https://res.cloudinary.com/dfdurnxxn/image/upload/v1769846463/guntas.dev/smklpnrrnunvj7f6qczd.png",
-    tags: ["Logo", "Email", "Web"],
-    video: null,
-  },
-  {
-    slug: "serenetile",
-    href: "https://www.guntassandhu.com/works/serenetile",
-    title: "Serene Marble & Tile",
-    description: "Premium tile install",
-    image:
-      "https://res.cloudinary.com/dfdurnxxn/image/upload/v1769847434/guntas.dev/zqn19cora8tkvpn8xo82.png",
-    tags: ["Branding", "Next.js", "UI"],
-    video: null,
-  },
-  {
-    slug: "anuragPhotography",
-    href: "https://www.guntassandhu.com/works/anuragPhotography",
-    title: "Anurag Photography",
-    description: "Premium Photography Website",
-    image:
-      "https://res.cloudinary.com/dfdurnxxn/image/upload/v1769948535/guntas.dev/ux9symdzvowtbcaddrrk.png",
-    tags: ["Next.js", "TypeScript", "Framer Motion"],
-    video: null,
-  },
-  {
-    slug: "the-bakers-kraft",
-    href: "https://www.guntassandhu.com/works/the-bakers-kraft",
-    title: "The Bakers Kraft",
-    description: "Elegant bakery brand",
-    image:
-      "https://res.cloudinary.com/dfdurnxxn/image/upload/v1769850948/guntas.dev/xraainszke6cpdyt3nnp.png",
-    tags: ["Branding", "Logo", "Website"],
-    video: null,
-  },
+  // {
+  //   slug: "smilinshack",
+  //   href: "https://www.guntassandhu.com/works/smilinshack",
+  //   title: "SmilinShack Booth",
+  //   description: "Modern photo booth site",
+  //   image:
+  //     "https://res.cloudinary.com/dfdurnxxn/video/upload/so_0,w_800,q_auto,f_webp/v1769846461/guntas.dev/i8f1zb7f8plofdofrchl.webp",
+  //   tags: ["Next.js", "SEO", "MDX"],
+  //   video: "/smilinshack.mp4",
+  // },
+  // {
+  //   slug: "hometechsystems",
+  //   href: "https://www.guntassandhu.com/works/hometechsystems",
+  //   title: "HomeTech Systems",
+  //   description: "Security automation web",
+  //   image:
+  //     "https://res.cloudinary.com/dfdurnxxn/video/upload/so_0,w_800,q_auto,f_webp/v1769846462/guntas.dev/ydn6l1lxtw2whuzekpb8.webp",
+  //   tags: ["Branding", "Next.js", "UI"],
+  //   video: "/hometechsystem.mp4",
+  // },
+  // {
+  //   slug: "srt-transport-inc",
+  //   href: "https://www.guntassandhu.com/works/srt-transport-inc",
+  //   title: "SRT Transport",
+  //   description: "Logistics business site",
+  //   image:
+  //     "https://res.cloudinary.com/dfdurnxxn/image/upload/v1769846463/guntas.dev/smklpnrrnunvj7f6qczd.png",
+  //   tags: ["Logo", "Email", "Web"],
+  //   video: null,
+  // },
+  // {
+  //   slug: "serenetile",
+  //   href: "https://www.guntassandhu.com/works/serenetile",
+  //   title: "Serene Marble & Tile",
+  //   description: "Premium tile install",
+  //   image:
+  //     "https://res.cloudinary.com/dfdurnxxn/image/upload/v1769847434/guntas.dev/zqn19cora8tkvpn8xo82.png",
+  //   tags: ["Branding", "Next.js", "UI"],
+  //   video: null,
+  // },
+  // {
+  //   slug: "anuragPhotography",
+  //   href: "https://www.guntassandhu.com/works/anuragPhotography",
+  //   title: "Anurag Photography",
+  //   description: "Premium Photography Website",
+  //   image:
+  //     "https://res.cloudinary.com/dfdurnxxn/image/upload/v1769948535/guntas.dev/ux9symdzvowtbcaddrrk.png",
+  //   tags: ["Next.js", "TypeScript", "Framer Motion"],
+  //   video: null,
+  // },
+  // {
+  //   slug: "the-bakers-kraft",
+  //   href: "https://www.guntassandhu.com/works/the-bakers-kraft",
+  //   title: "The Bakers Kraft",
+  //   description: "Elegant bakery brand",
+  //   image:
+  //     "https://res.cloudinary.com/dfdurnxxn/image/upload/v1769850948/guntas.dev/xraainszke6cpdyt3nnp.png",
+  //   tags: ["Branding", "Logo", "Website"],
+  //   video: null,
+  // },
   {
     slug: "sbssu-library-portal",
     href: "https://www.guntassandhu.com/works/sbssu-library-portal",
